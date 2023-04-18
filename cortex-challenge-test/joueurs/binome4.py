7dbef7f51511c6ae6ed6fcf93349f282f1919c77
Steeex <chameauq@gmail.com>
2023-04-18 20:59:42 +0200

    Ajout des fichiers initiaux

---
+++ cortex-challenge-test/joueurs/binome4.py
@@ -0,0 +1,19 @@
+"""
+L1 - MI-I : Concrétisation disciplinaire -- Cortex Challenge
+"""
+
+__team__ = "Binôme 4"
+__authors__ = ["Prénom NOM", "Prénom NOM"]
+__date__ = "16/02/2023"
+__version__ = "0.0"
+
+from typing import Union
+
+
+def jouer(card: dict) -> Union[None, str, int]:
+    '''
+    Fonction commune à tous les groupes permettant de jouer tous ensemble
+    :param card: la carte au format JSON
+    :return: le résultat trouvé par son joueur. Si type non implémenté return None.
+    '''
+    return None
