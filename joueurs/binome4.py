c30d2ae64d6fd0b6428b1600c8b25008050ee800
Steeex <chameauq@gmail.com>
2023-04-18 23:20:54 +0200

    Ajout des algorithmes dans script joueur

--- joueurs/binome4.py
+++ joueurs/binome4.py
@@ -3,12 +3,18 @@ L1 - MI-I : Concrétisation disciplinaire -- Cortex Challenge
 """
 
 __team__ = "Binôme 4"
-__authors__ = ["Prénom NOM", "Prénom NOM"]
-__date__ = "16/02/2023"
-__version__ = "0.0"
+__authors__ = ["Antoine DUMAIT", "Islem HOCINI"]
+__date__ = "18/04/2023"
+__version__ = "0.1"
 
 from typing import Union
-
+import algo.couleur as couleur
+import algo.reflexion as reflexion
+import algo.frequence as frequence
+import algo.manquant as manquant
+import algo.doublon as doublon
+import algo.raisonnement as raisonnement
+import algo.calcul as calcul
 
 def jouer(card: dict) -> Union[None, str, int]:
     '''
@@ -16,4 +22,14 @@ def jouer(card: dict) -> Union[None, str, int]:
     :param card: la carte au format JSON
     :return: le résultat trouvé par son joueur. Si type non implémenté return None.
     '''
+    type = card.get("type", None)
+   
+    if type == "couleur": return couleur.main(card)
+    if type == "réflexion": return reflexion.main(card)
+    if type == "fréquence": return frequence.main(card)
+    if type == "manquant": return manquant.main(card)
+    if type == "doublon": return doublon.main(card)
+    if type == "raisonnement": return raisonnement.main(card)
+    if type == "calcul": return calcul.main(card)
+    
     return None
